8dcb005a5af3d9de48392dee404988ba7abf51d1
XYPMA-11 <danilkop2000@mail.ru>
2022-11-16 17:08:43 +0500

    перемещение камеры по локации

--- ProjectA/Assets/Scripts/CameraMove.cs
+++ ProjectA/Assets/Scripts/CameraMove.cs
@@ -1,59 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
     public GameObject player;
 
-    private Vector3 startPoint;
+    private BoxCollider2D borderScreen;
     // Start is called before the first frame update
     void Start()
     {
-        startPoint = player.transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        //gameObject.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
+        borderScreen = GetComponent<BoxCollider2D>();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Debug.Log(startPoint);
-            Debug.Log(player.transform.position.x - startPoint.x);
-            Debug.Log(player.transform.position.y - startPoint.y);
-            if (Mathf.Abs(player.transform.position.x - startPoint.x) >= 13)
+            if (player.transform.position.x - gameObject.transform.position.x > (borderScreen.size.x / 2))
             {
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x + 18, gameObject.transform.position.y, gameObject.transform.position.z);
-                startPoint = player.transform.position;
             }
-            else if (Mathf.Abs(player.transform.position.x - startPoint.x) <= 2)
+
+            else if (Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) > (borderScreen.size.x / 2))
             {
-                if (player.transform.position.x - startPoint.x < 0)
-                    gameObject.transform.position = new Vector3(gameObject.transform.position.x - 18, gameObject.transform.position.y, gameObject.transform.position.z);
-                else
-                    gameObject.transform.position = new Vector3(gameObject.transform.position.x + 18, gameObject.transform.position.y, gameObject.transform.position.z);
-                startPoint = player.transform.position;
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x - 18, gameObject.transform.position.y, gameObject.transform.position.z);
             }
-            else if (Mathf.Abs(player.transform.position.y - startPoint.y) >= 5)
+
+            else if (player.transform.position.y > gameObject.transform.position.y)
             {
                 gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z);
-                startPoint = player.transform.position;
             }
-            else if (Mathf.Abs(player.transform.position.y - startPoint.y) <= 4)
+
+            else if (player.transform.position.y < gameObject.transform.position.y)
             {
-                if (player.transform.position.y - startPoint.y < 0)
-                    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 10, gameObject.transform.position.z);
-                else
-                    gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z);
-                startPoint = player.transform.position;
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 10, gameObject.transform.position.z);
             }
         }
     }
-
 }
